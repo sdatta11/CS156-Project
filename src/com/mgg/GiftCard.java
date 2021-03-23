df35ecb8ef98b6ab7a6eb99fc1cc9974b8f38705
sdatta11 <sdatta11@huskers.unl.edu>
2021-03-23 17:53:15 -0500

    GiftCard class

---
+++ src/com/mgg/GiftCard.java
@@ -0,0 +1,15 @@
+package com.mgg;
+
+public class GiftCard extends Product {
+
+	public GiftCard(Product p, double quantity) {
+		super(p, quantity);
+		// TODO Auto-generated constructor stub
+	}
+
+	public GiftCard(String code, String type, String name, double basePrice) {
+		super(code, type, name, basePrice);
+		// TODO Auto-generated constructor stub
+	}
+	
+}
