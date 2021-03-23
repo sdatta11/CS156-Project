f90b3bb7f62075493cf17905e8fd766c4bfc4645
sdatta11 <sdatta11@huskers.unl.edu>
2021-03-23 17:37:18 -0500

    Started UsedProduct class

---
+++ src/com/mgg/UsedProduct.java
@@ -0,0 +1,10 @@
+package com.mgg;
+
+public class UsedProduct extends Product {
+
+	public UsedProduct(String code, String type, String name, double basePrice) {
+			super(code, type, name, basePrice);
+		}
+
+		
+}
