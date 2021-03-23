727a7175839255d065bef71214a1dbf19e9046e2
jacob <jacob@DESKTOP-C7OPVFK.unl.edu>
2021-03-23 17:31:56 -0500

    added constructor to newproduct class

--- src/com/mgg/NewProduct.java
+++ src/com/mgg/NewProduct.java
@@ -2,5 +2,11 @@ package com.mgg;
 
 public class NewProduct extends Product{
 
+	public NewProduct(String code, String type, String name, double basePrice) {
+		super(code, type, name, basePrice);
+	}
+
+	
+
 	//TODO: implement
 }
